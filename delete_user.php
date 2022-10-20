8264c11cf65a1ec843830045d8f8c50146a6cdf0
DoTu <dominhtu81@gmail.com>
2022-10-20 14:52:25 +0700

    sua loi CSRF

--- delete_user.php
+++ delete_user.php
@@ -1,13 +1,23 @@
 <?php
+session_start();
+
 require_once 'models/UserModel.php';
 $userModel = new UserModel();
 
 $user = NULL; //Add new user
 $id = NULL;
 
-if (!empty($_GET['id'])) {
+$token = $_GET['token'];
+if ($_SESSION['token_ss'] == $token ) {
     $id = $_GET['id'];
     $userModel->deleteUserById($id);//Delete existing user
+}else{
+    header('location: list_users.php');
 }
+
+// if (!empty($_GET['id'])) {
+//     $id = $_GET['id'];
+//     $userModel->deleteUserById($id);//Delete existing user
+// }
 header('location: list_users.php');
 ?>
